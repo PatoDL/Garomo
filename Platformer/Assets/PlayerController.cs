40da9ffc10228dd858167674e687092b988cde87
Pato <patodl2701@gmail.com>
2019-09-05 13:06:27 -0300

    Character Improved

--- Platformer/Assets/PlayerController.cs
+++ Platformer/Assets/PlayerController.cs
@@ -13,12 +13,18 @@ public class PlayerController : MonoBehaviour
 
     bool lookingRight;
 
+    public bool jumping;
+
+    public float distance;
     void Start()
     {
+
         rig = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         lookingRight = true;
+        jumping = false;
+        Physics2D.gravity = new Vector2(0,-40f);
     }
 
     void Update()
@@ -48,6 +54,25 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("RunningR", false);
         }
+
+        Vector3 origin = new Vector3(transform.position.x-transform.localScale.x/2,transform.position.y,transform.position.z);
+        Vector3 origin2 = new Vector3(transform.position.x + transform.localScale.x / 2, transform.position.y, transform.position.z);
+
+        RaycastHit2D[] raycastHit2D = new RaycastHit2D[2];
+
+        raycastHit2D[0] = Physics2D.Raycast(origin, Vector2.down * distance);
+        raycastHit2D[1] = Physics2D.Raycast(origin2, Vector2.down * distance);
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (raycastHit2D[i])
+            {
+                if (raycastHit2D[i].transform.tag == "floor" && jumping)
+                    jumping = false;
+            }
+        }
+
+        Debug.DrawRay(origin, Vector2.down * distance);
     }
 
     void FixedUpdate()
@@ -55,9 +80,27 @@ public class PlayerController : MonoBehaviour
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
-        if(hor!=0)
+        if (hor == 0)
+        {
+            Invoke("ReserVelocity", 0.2f);
+        }
+        else
+        {
+            CancelInvoke("ReserVelocity");
             rig.velocity = new Vector2(hor * velX * Time.fixedDeltaTime, rig.velocity.y);
-        if(ver!=0)
-            rig.velocity = new Vector2(rig.velocity.x, ver * velY * Time.fixedDeltaTime);
+        }
+     
+
+        if (!jumping && ver > 0)
+        {
+            rig.AddForce(new Vector2(0, 20f), ForceMode2D.Impulse);
+            jumping = true;
+        }
+    }
+
+    public void ReserVelocity()
+    {
+        rig.velocity = new Vector2(0, rig.velocity.y);
+
     }
 }
