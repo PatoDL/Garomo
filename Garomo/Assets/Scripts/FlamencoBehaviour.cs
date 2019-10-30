f78b1f5e0ce0f70cc536e0df92ebcf29379a1722
Pato <patodl2701@gmail.com>
2019-10-30 02:56:02 -0300

    Revert "Added new scene and the sew pass trigger"
    This reverts commit 5ebec04ac00ddc6b4910df95aef204c8c2674b70.

---
+++ Garomo/Assets/Scripts/FlamencoBehaviour.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamencoBehaviour : MonoBehaviour
+{
+    public int life;
+
+    public Vector3 direction;
+
+    public float speed;
+
+    float changeDirTimer = 0;
+
+    private float normalizedHorizontalSpeed = 0;
+
+    public float shotTimerMax = 2f;
+    public float shotTimer = 2f;
+    public GameObject shot;
+
+    Rigidbody2D rig;
+    Animator anim;
+    SpriteRenderer spr;
+    BoxCollider2D col;
+
+    public Sprite deadBody;
+
+    public bool dead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!dead)
+        {
+            shotTimer -= Time.deltaTime;
+            changeDirTimer += Time.deltaTime;
+
+            rig.velocity = new Vector3(direction.x * speed * Time.deltaTime, 0, 0);
+
+            if (changeDirTimer > 5)
+            {
+                changeDirTimer = 0;
+                direction = -direction;
+                transform.localScale = new Vector3(-direction.x, 1, 1);
+            }
+
+            if (shotTimer <= 0)
+            {
+                shotTimer = shotTimerMax;
+                anim.SetTrigger("Attack");
+            }
+        }
+        else
+        {
+            rig.velocity += Vector2.down * speed * Time.deltaTime;
+        }
+    }
+
+    void Shot()
+    {
+        GameObject go = Instantiate(shot);
+        go.transform.position = transform.position;
+        go.GetComponent<Rigidbody2D>().velocity = new Vector3(0, -speed * 3/2 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Attack")
+        {
+            life -= 10;
+            if((collision.gameObject.transform.position.x < transform.position.x && transform.localScale.x < 0f) || 
+                (collision.gameObject.transform.position.x > transform.position.x && transform.localScale.x > 0f))
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+            }
+            anim.SetTrigger("Damage");
+            rig.velocity = Vector3.zero;
+            if(life <= 0f)
+            {
+                speed = 500f;
+                spr.sprite = deadBody;
+                col.isTrigger = true;
+                anim.enabled = false;
+                dead = true;
+            }
+        }
+        else if (collision.transform.tag == "LimitTrigger")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void ContinueIdle()
+    {
+        rig.velocity = new Vector3(direction.x * speed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Garomo/Assets/Scripts/GaromoController.cs
@@ -0,0 +1,368 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+
+public class GaromoController : MonoBehaviour
+{
+	// movement config
+	public float gravity = -25f;
+	public float runSpeed = 8f;
+	public float groundDamping = 20f; // how fast do we change direction? higher means faster
+	public float inAirDamping = 5f;
+	public float jumpHeight = 3f;
+
+    public int life = 100;
+
+    public float recoil = 0f;
+
+    public float rollSpeed = 0f;
+
+    bool enemyCollision = false;
+    bool isRecoiling = false;
+    public float recoilTimeMax = 3.0f;
+    public float recoilTime = 3.0f;
+
+    [HideInInspector]
+	private float normalizedHorizontalSpeed = 0;
+
+	private CharacterController2D _controller;
+	public Animator _animator;
+	private RaycastHit2D _lastControllerColliderHit;
+	private Vector3 _velocity;
+
+    bool rolled = false;
+    float rollTimer = 0.0f;
+
+    private bool immunity = false;
+    private float enemyCollisionTimer = 0.0f;
+
+    public BoxCollider2D crouchCollider;
+    public BoxCollider2D idleCollider;
+    public BoxCollider2D GroundAttackCollider;
+    public BoxCollider2D AirAttackCollider;
+    bool canMove = true;
+
+    public bool isRolling = false;
+    public float rollDistance = 0f;
+
+    Vector3 startPos;
+
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_controller = GetComponent<CharacterController2D>();
+        idleCollider = _controller.boxCollider;
+		// listen to some events for illustration purposes
+		_controller.onControllerCollidedEvent += onControllerCollider;
+		_controller.onTriggerEnterEvent += onTriggerEnterEvent;
+		_controller.onTriggerExitEvent += onTriggerExitEvent;
+
+
+
+        startPos = transform.position;
+	}
+
+
+	#region Event Listeners
+
+	void onControllerCollider( RaycastHit2D hit )
+	{
+		// bail out on plain old ground hits cause they arent very interesting
+		if( hit.normal.y == 1f )
+			return;
+
+        //if((hit.transform.tag == "Enemy" || hit.transform.tag == "Obstacle") && !immunity)
+        //{
+        //    enemyCollision = immunity = true;
+            
+        //}
+
+        // logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
+        //Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+    }
+
+
+	void onTriggerEnterEvent( Collider2D col )
+	{
+        if(col.tag=="Enemy")
+            enemyCollision = immunity = true;
+    }
+
+
+	void onTriggerExitEvent( Collider2D col )
+	{
+		//Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+	}
+
+	#endregion
+
+
+	// the Update loop contains a very simple example of moving the character around and controlling the animation
+	void Update()
+	{
+        if(immunity)
+        {
+            enemyCollisionTimer += Time.deltaTime;
+            if(enemyCollisionTimer>1.5f)
+            {
+                immunity = false;
+                enemyCollisionTimer = 0.0f;
+            }
+        }
+
+        if(rolled)
+        {
+            rollTimer += Time.deltaTime;
+            if(rollTimer>1.5f)
+            {
+                rollTimer = 0f;
+                rolled = false;
+            }
+        }
+
+        if (_controller.isGrounded)
+        {
+            _velocity.y = 0;
+            _animator.SetBool("Jumping", false);
+        }
+        
+        if (canMove)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                normalizedHorizontalSpeed = 1;
+                if (transform.localScale.x < 0f)
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+                if (_controller.isGrounded)
+                    _animator.SetBool("Running", true);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                normalizedHorizontalSpeed = -1;
+                if (transform.localScale.x > 0f)
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+                if (_controller.isGrounded)
+                    _animator.SetBool("Running", true);
+            }
+            else
+            {
+                normalizedHorizontalSpeed = 0;
+                _animator.SetBool("Running", false);
+            }
+        }
+        else
+        {
+            normalizedHorizontalSpeed = 0;
+            _animator.SetBool("Running", false);
+            if(!_controller.isGrounded && recoil>25)
+            {
+                if (_velocity.x > 0)
+                    _velocity.x = 1;
+                else
+                    _velocity.x = -1;
+
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            _animator.SetTrigger("Punch");
+        }
+
+		// we can only jump whilst grounded
+		if( _controller.isGrounded && Input.GetKeyDown( KeyCode.UpArrow ) )
+		{
+            Jump();
+            _animator.SetBool("Jumping", true);
+            _animator.SetBool("Running", false);
+        }
+
+        if(!_controller.isGrounded && _controller.nearFloor)
+        {
+            _animator.SetBool("Jumping", false);
+            _animator.SetBool("Falling", true);
+            _animator.SetBool("Running", false);
+        }
+
+        if(enemyCollision)
+        {
+            _animator.SetTrigger("Damage");
+            enemyCollision = false;
+
+            if (_controller.isGrounded)
+                recoil = 1500;
+            else
+                recoil = 25;
+
+            //if (_controller.collidedLeft)
+            //{
+            //    _velocity.x += recoil;
+            //}
+            //else if(_controller.collidedRight)
+            //{
+            //    _velocity.x -= recoil;
+            //}
+
+            isRecoiling = true;
+            canMove = false;
+            recoilTime = recoilTimeMax;
+        }
+
+        if(isRecoiling)
+        {
+            recoilTime -= Time.deltaTime;
+            Recoil();
+            if(recoilTime<=0f)
+            {
+                isRecoiling = false;
+                canMove = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isRolling = true;
+            _controller.ignoreSlopeModifier = true;
+            _animator.SetTrigger("Roll");
+            rollTimer = rollDistance;
+        }
+
+        if (isRolling)
+        {
+            Roll();
+            rollTimer -= Time.deltaTime;
+            if (rollTimer <= 0.0f)
+            {
+                isRolling = false;
+                _controller.ignoreSlopeModifier = false;
+                rollTimer = rollDistance;
+            }
+        }
+
+        // apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
+        if (!isRolling || canMove)
+        {
+            var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+            _velocity.x = Mathf.Lerp(_velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor);
+        }
+
+		// apply gravity before moving
+		_velocity.y += gravity * Time.deltaTime;
+
+        // if holding down bump up our movement amount and turn off one way platform detection for a frame.
+        // this lets us jump down through one way platforms
+        if (_controller.isGrounded && Input.GetKey(KeyCode.DownArrow))
+        {
+            if (_controller.boxCollider != crouchCollider)
+            { 
+                _controller.boxCollider = crouchCollider;
+                _controller.recalculateDistanceBetweenRays();
+                _animator.SetBool("Crouching", true);
+            }
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                if (_controller.boxCollider != idleCollider)
+                {
+                    _controller.boxCollider = idleCollider;
+                    _controller.recalculateDistanceBetweenRays();
+                    _animator.SetBool("Jumping", true);
+                    _animator.SetBool("Crouching", false);
+                    _animator.SetBool("Jumping", false);
+                }
+            }
+		}
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            if (_controller.boxCollider != idleCollider)
+            {
+                _controller.boxCollider = idleCollider;
+                _controller.recalculateDistanceBetweenRays();
+                _animator.SetBool("Crouching", false);
+            }
+        }
+
+        _controller.move( _velocity * Time.deltaTime );
+
+        //Debug.Log(_controller.onSlope);
+
+        // grab our current _velocity to use as a base for all calculations
+        _velocity = _controller.velocity;
+	}
+
+    public void Restart()
+    {
+        transform.position = new Vector3(-380f,-1f,0f);
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.tag == "Trampoline")
+        {
+            _velocity.y = Mathf.Sqrt(8f * jumpHeight * -gravity);
+            _animator.SetBool("Jumping", true);
+            _animator.SetBool("Running", false);
+            _controller.move(_velocity * Time.deltaTime);
+        }
+        else if(col.transform.tag == "LimitTrigger")
+        {
+            transform.position = startPos;
+            _velocity.y = 0;
+            _controller.move(_velocity * Time.deltaTime);
+        }
+        else if (col.transform.tag == "EndlevelTrigger")
+        {
+            Debug.Log("Ganaste");
+        }
+    }
+
+    public void ActiveCrouchCollider()
+    {
+        _controller.boxCollider = crouchCollider;
+        _controller.recalculateDistanceBetweenRays();
+    }
+
+    public void DeActiveCrouchCollider()
+    {
+        _controller.boxCollider = idleCollider;
+        _controller.recalculateDistanceBetweenRays();
+    }
+
+    public void Jump()
+    {
+        _velocity.y = Mathf.Sqrt(4f * jumpHeight * -gravity);
+    }
+
+    void ValidateMovement()
+    {
+        canMove = true;
+    }
+
+    public void TeleportTo(Transform t)
+    {
+        transform.position = t.position;
+    }
+
+    public void ActivePunch()
+    {
+        GroundAttackCollider.gameObject.SetActive(true);
+    }
+
+    public void DeActivePunch()
+    {
+        GroundAttackCollider.gameObject.SetActive(false);
+    }
+
+    public void Roll()
+    {  
+        _velocity.x += transform.localScale.x * rollSpeed * Time.deltaTime;
+    }
+
+    public void Recoil()
+    {
+        _velocity.x -= transform.localScale.x * recoil * Time.deltaTime;
+    }
+}
