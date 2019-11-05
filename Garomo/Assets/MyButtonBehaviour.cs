6e4410eeeff3ccc244722b70f557257679c25e9f
Pato <patodl2701@gmail.com>
2019-11-04 22:19:25 -0300

    UI Button Bugs Solved

---
+++ Garomo/Assets/MyButtonBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MyButtonBehaviour : EventTrigger
+{
+    Vector3 originalSize;
+
+    private void Start()
+    {
+        originalSize = transform.localScale;
+    }
+
+    public override void OnPointerEnter(PointerEventData data)
+    {
+        transform.localScale *= 1.2f;
+    }
+
+    public override void OnPointerExit(PointerEventData data)
+    {
+        transform.localScale /= 1.2f;
+    }
+
+    private void OnDisable()
+    {
+        transform.localScale = originalSize;
+    }
+}
